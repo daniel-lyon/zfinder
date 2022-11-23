710e4a6c8848c1204ac4205b09a8e368e239724a
Daniel Lyon <92438910+daniel-lyon@users.noreply.github.com>
2022-11-23 15:17:17 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 # Required functions
-import numpy as np
-import matplotlib.pyplot as plt
 from astropy.io import fits
 from photutils.aperture import CircularAperture, aperture_photometry
 from photutils.background import Background2D
@@ -15,12 +13,13 @@ data = fits_image[0].data[0][0] # data was size (400, 400, 1, 1) -> converted to
 # Required header values
 bmaj = hdr['BMAJ']
 bmin = hdr['BMIN']
-pix2deg = hdr['CDELT2'] # unit conversion
+pix2deg = hdr['CDELT1'] # unit conversion
+print(pix2deg)
 
 # Aperture setup
-data_dimensions = np.shape(data) # shape of the data sturecture (x,y)
-radius = round(1/pix2deg/3600) # number of pixels in one arcsecond -> 5
-position = (192, 198) # centre of the image (192, 198)
+# radius = round(1/pix2deg/3600) # number of pixels in one arcsecond = 5
+radius = 3
+position = (192, 198) # centre of the aperture
 aperture = CircularAperture(position, radius)
 
 # Background data
@@ -30,12 +29,8 @@ bkg = Background2D(data, position).background
 apsum = aperture_photometry(data - bkg, aperture)
 apsum = apsum['aperture_sum'][0]
 
-# Calculate beam area
-barea = 1.1331 * bmaj * bmin
-
 # Corrected flux
+barea = 1.1331 * bmaj * bmin # beam area
 total_flux = apsum*(pix2deg**2)/barea
 print()
-print(f'.fits total image flux = {total_flux}')
-print()
-
+print(f'.fits total image flux = {total_flux*1000} mJy')
