8deae8686c29006169cf279a5594eab94455af6f
Daniel Lyon <92438910+daniel-lyon@users.noreply.github.com>
2022-11-23 10:57:41 +0800

    fits source flux measurement

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Import functions
+# Required functions
 import numpy as np
 import matplotlib.pyplot as plt
 from astropy.io import fits
@@ -9,24 +9,24 @@ fits_image = fits.open('0856.fits.image.tt0.fits')
 
 # Get data
 hdr = fits_image[0].header 
-data = fits_image[0].data[0][0] # data was size (400, 400, 1, 1)
+data = fits_image[0].data[0][0] # data was size (400, 400, 1, 1) -> converted to (400, 400)
 
 # Aperture setup
-data_shape = np.shape(data) # shape of the data sturecture (x,y)
-radius = data_shape[0]/2 # radius of the circular aperture (200)
+data_dimensions = np.shape(data) # shape of the data sturecture (x,y)
+radius = data_dimensions[0]/2 # radius of the circular aperture (200)
 position = (radius, radius) # centre of the image (200, 200)
 aperture = CircularAperture(position, radius)
 
-# Flux of the fits image
-total_flux = aperture_photometry(data, aperture)['aperture_sum'][0]
-print(total_flux)
+# Aperture sum of the fits image
+apsum = aperture_photometry(data, aperture)['aperture_sum'][0]
 
+# Calculate beam area
 bmaj = hdr['BMAJ']
 bmin = hdr['BMIN']
-#print(bmaj, bmin)
-
-
-
-
 barea = 1.1331 * bmaj * bmin
+
+# Corrected flux
+pix2deg = hdr['CDELT1'] # unit conversion of apsum to barea units
+total_flux = apsum*(pix2deg**2)/barea
+print(total_flux)
 
