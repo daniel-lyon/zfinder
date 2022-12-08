c649d6aa5e142f0e98ae7f0083b9f4f5b3c8cc13
Daniel Lyon <92438910+daniel-lyon@users.noreply.github.com>
2022-12-08 09:40:36 +0800

    Updated example usage

--- example.py
+++ example.py
@@ -9,7 +9,7 @@ bvalue = 3 # BMAJ & BMIN (arcseconds)
 
 # Spaced Points setup
 min_sep = 5 # Minimum separation between points (pixels)
-num_plots = 9 # Number of plots to make (#, must be a square number)
+num_plots = 4 # Number of plots to make (#, must be a square number or 1)
 circle_radius = 88 # Radius of the largest frequency (pixels)
 
 # Redshift variables to iterate through
@@ -20,7 +20,17 @@ z_end = 10 # final redshift
 
 zfind1 = zf(filename, ra, dec, aperture_radius, bvalue)
 zfind1.circlePointVars(min_sep, num_plots, circle_radius)
-chi2, z, coords, colours, yFlux, xFlux = zfind1.zfind(ftransition, z_start, dz, z_end, timer=True)
+zfind1.zfind(ftransition, z_start, dz, z_end, timer=True)
+
+# Can use these arrays to create your own plots
+all_chi2 = zfind1.allChi2
+redshift_array = zfind1.z
+plot_points = zfind1.coordinates
+plot_colours = zfind1.plotColours
+all_flux_y = zfind1.allFlux
+all_flux_x = zfind1.xAxisFlux
+
+# Otherwise, can use built in functions
 zfind1.plotCoords()
 zfind1.plotChi2()
 zfind1.plotFlux()
