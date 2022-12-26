628aa00e9942cf83ae1c7c9d1aceadb3d4b61089
Daniel Lyon <92438910+daniel-lyon@users.noreply.github.com>
2022-12-26 13:29:43 +1000

    small changes - up to date

--- example1.py
+++ example1.py
@@ -8,11 +8,11 @@ from zfinder import RedshiftFinder as zf, RedshiftPlotter as zp
 
 # Required variables
 image = '0856_cube_c0.4_nat_80MHz_taper3.fits'
-ra = [8, 56, 14.8] # Right Ascenion (h, m, s) orig_ra = [8, 56, 14.73]
-dec = [2, 24, 0.6, 1] # Declination (d, m, s, sign)  orig_dec = [2, 23, 59.6, 1]
+ra = [8, 56, 14.8] # Right Ascenion (h, m, s)
+dec = [2, 24, 0.6, 1] # Declination (d, m, s, sign)
 aperture_radius = 3 # Aperture Radius (pixels)
 bvalue = 3 # BMAJ & BMIN (arcseconds)
-ftransition = 115.2712 # the first transition in GHz (this is a CO Molecule)
+ftransition = 115.2712 # the first transition in {unit}Hz (this is a CO)
 
 # Find the redshift
 zf1 = zf(image, ra, dec, aperture_radius, bvalue)
@@ -21,4 +21,4 @@ print(f'The redshift of this source is z={z[0]}')
 
 zp1 = zp(zf1)
 zp1.plot_chi2() # Plot chi-squared vs z
-zp1.plot_flux() # plot flux vs frequency
+zp1.plot_flux() # Plot flux vs frequency
