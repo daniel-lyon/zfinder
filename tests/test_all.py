cc06311b1b95bb9d47ee85e90e91c0c21a5e5387
Daniel Lyon <daniellyon31@gmail.com>
2023-09-17 08:02:48 +1000

    added testing functionality

---
+++ tests/test_all.py
@@ -0,0 +1,40 @@
+# Import packages
+import pytest
+import numpy as np
+import matplotlib.pyplot as plt
+
+import sys
+print(sys.path)
+
+from zfinder.fits2flux import Fits2flux
+from zfinder.zfind import zfind
+
+def test_main():
+    fitsfile = 'zfinder/0856_cube_c0.4_nat_80MHz_taper3.fits'
+    ra = '08:56:14.8'
+    dec = '02:24:00.6'
+    aperture_radius = 3
+    transition = 115.2712
+
+    source = Fits2flux(fitsfile, ra, dec, aperture_radius)
+    freq = source.get_freq()
+    flux, flux_uncert = source.get_flux()
+
+    finder = zfind(transition, freq, flux, flux_uncert)
+    z, chi2 = finder.zfind()
+
+    lowest_index = np.argmin(chi2)
+    best_fit_redshift = z[lowest_index]
+    print(best_fit_redshift)
+
+    assert best_fit_redshift == pytest.approx(5.55)
+
+    # plt.plot(z, chi2)
+    # plt.show()
+
+    # plt.plot(freq, flux, color='black', drawstyle='steps-mid')
+    # plt.fill_between(freq, flux, 0, where=(np.array(flux) > 0), color='gold', alpha=0.75)
+    # plt.show()
+
+if __name__ == '__main__':
+    test_main()
