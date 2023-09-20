b7ea14810ae03b239690340f2d7c58316f0c4ab9
Daniel Lyon <daniellyon31@gmail.com>
2023-09-20 20:59:06 +1000

    touch ups

--- zfinder/tests/test_fft.py
+++ zfinder/tests/test_fft.py
@@ -1,32 +1,33 @@
 import pytest
     
 import numpy as np
-import matplotlib.pyplot as plt
 
 from zfinder.fits2flux import Fits2flux
 from zfinder.fft import fft_zfind
 
 def test_fft():
+    """ Test the fft method """
+
+    # Load in the data
     fitsfile = 'zfinder/0856_cube_c0.4_nat_80MHz_taper3.fits'
     ra = '08:56:14.8'
     dec = '02:24:00.6'
     aperture_radius = 3
     transition = 115.2712
 
+    # Calculate the frequency and flux
     source = Fits2flux(fitsfile, ra, dec, aperture_radius)
     freq = source.get_freq()
     flux, flux_uncert = source.get_flux()
 
+    # Calculate the fft chi2
     z, chi2 = fft_zfind(transition, freq, flux)
 
+    # Find the best fit redshift
     lowest_index = np.argmin(chi2)
     best_fit_redshift = z[lowest_index]
-    print(best_fit_redshift)
 
     assert best_fit_redshift == pytest.approx(5.55)
-
-    plt.plot(z, chi2)
-    plt.show()
 
 if __name__ == '__main__':
     test_fft()
