f583f8564b0f14ed176e4835a8908e21d781a76d
Daniel Lyon <92438910+daniel-lyon@users.noreply.github.com>
2023-10-25 16:37:15 +1000

    update conf

--- docs/conf.py
+++ docs/conf.py
@@ -8,10 +8,11 @@
 
 import os
 import sys
-import sphinx_rtd_theme
-import zfinder
 
 sys.path.insert(0, os.path.abspath('..'))
+
+import sphinx_rtd_theme
+import zfinder
 
 project = 'zfinder'
 copyright = '2023, Daniel Lyon'
