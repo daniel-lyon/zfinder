db95fbca4344909aefa65beccdca2b459c7ff9c1
Daniel Lyon <92438910+daniel-lyon@users.noreply.github.com>
2024-02-08 09:23:00 +1000

    quick patch

--- setup.py
+++ setup.py
@@ -1,19 +1,29 @@
 from setuptools import setup, find_packages
+import re
+import os
+import codecs
 
-VERSION = '1.0.2'
-DESCRIPTION = 'Redshift finding algorithm'
-LONG_DESCRIPTION = 'Find the redshift of high redshift radio galaxies'
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'zfinder/__init__.py'), 'r') as f:
+    VERSION = re.search(r"__version__ = '(.+)'", f.read()).group(1)
+
+with open(os.path.join(here, 'requirements.txt'), 'r') as f:
+    requirements = f.read().splitlines()
+    
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    LONG_DESCRIPTION = "\n" + fh.read()
 
 setup(
     name="zfinder",
     version=VERSION,
     author="Daniel Lyon",
     author_email="daniellyon31@gmail.com",
-    description=DESCRIPTION,
+    description='Redshift finding algorithm',
     long_description_content_type="text/markdown",
     long_description=LONG_DESCRIPTION,
     packages=find_packages(),
-    install_requires=['numpy', 'matplotlib', 'scipy', 'astropy', 'photutils', 'PyAstronomy', 'sslf', 'radio_beam', 'tqdm'],
+    install_requires=requirements,
     keywords=['python', 'visualisation', 'redshift', 'galaxy-evolution', 'cosmology', 
               'epoch-of-reionisation', 'radio-galaxies', 'active-galactic-nuclei',
               'high-redshift', 'galaxies', 'black-holes'],
